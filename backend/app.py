4a8d0f2bf8409273db51508520fbd325b780a051
Shivank Rastogi <shivankrastogi4@gmail.com>
2026-03-30 17:23:58 +0530

    fix import os position

--- backend/app.py
+++ backend/app.py
@@ -143,6 +143,6 @@ def get_exams():
 
 if __name__ == '__main__':
     print("\n🚀 GovPrep India is running!")
-   import os
+    print("📍 Visit: http://127.0.0.1:5000\n")
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=False, host='0.0.0.0', port=port)
